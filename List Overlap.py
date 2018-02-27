17db2b49283efc21f4782d6effce0294ced4055a
Alessandro Di Micoli <adimicoli@gmail.com>
2018-02-27 16:57:13 +0000

    Divisors updated

---
+++ List Overlap.py
@@ -0,0 +1,21 @@
+'''
+http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+
+Take two lists, say for example these two:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras:
+
+1. Randomly generate two lists to test this
+2. Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+'''
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for x in a:
+    if x in b:
+        print(x)
